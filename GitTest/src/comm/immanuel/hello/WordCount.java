a14e19944e3d3b85c1f6a6a1acca54335f616864
immanuel <immanuel@example.com>
2018-05-16 11:12:28 +0530

    Word Count First Commit

---
+++ GitTest/src/comm/immanuel/hello/WordCount.java
@@ -0,0 +1,32 @@
+package comm.immanuel.hello;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class WordCount {
+
+	public static void main(String[] args) {
+		try {
+			File file = new File("/home/immanuel/Downloads/README");
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			StringBuffer stringBuffer = new StringBuffer();
+			String line;
+			int lcount=0;
+			while ((line = bufferedReader.readLine()) != null) {
+				stringBuffer.append(line);
+				stringBuffer.append("\n");
+				lcount++;
+				}
+			fileReader.close();
+			System.out.println(lcount);
+			System.out.println("Contents of file:");
+			System.out.println(stringBuffer.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+}
+
+}
